6b5fbbc7c35fdbe9ad551a86d8325522839e7962
Ache <ache.cg@gmail.com>
2019-10-31 15:40:32 -0500

    error del linkventa

--- controllers/admin/AdminVenderController.php
+++ controllers/admin/AdminVenderController.php
@@ -1272,7 +1272,8 @@ class AdminVenderControllerCore extends AdminController {
         die(json_encode(array(
             'success' => "ok",
             'result' => "Guardado",
-            'order' => $order
+            'order' => $order,
+            'link_venta' => $this->context->link->getAdminLink('AdminOrders').'&vieworder&id_order='.$order->id
         )));
 
     }
